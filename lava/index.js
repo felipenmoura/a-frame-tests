f9d8903ff510725d42546eac1b573b737c080bf2
Felipe <felipenmoura@gmail.com>
2018-05-20 14:55:01 -0300

    A first example with some lava effect

---
+++ lava/index.js
@@ -0,0 +1,34 @@
+window.addEventListener('load', function () {
+    var sceneEl = document.querySelector('a-scene');
+    var cubes = Array.from(sceneEl.querySelectorAll('.cube'))
+    cubes.forEach(cube => {
+      // cube.on('click', cubeClicked)
+      cube.addEventListener('click', cubeClicked);
+      cube.addEventListener('mouseenter', cubeHovered);
+      cube.addEventListener('mouseleave', cubeLeft);
+    })
+  })
+  
+  function cubeClicked () {
+    // alert('OMG')
+  }
+  function cubeHovered () {
+    // this.setAttribute('prev-color', this.getAttribute('color'))
+    // this.setAttribute('color', '#fff')
+    // const anim = document.createElement('a-animation')
+    // anim.id = "hover-animation-el"
+    // anim.attribute = "material.color"
+    // anim.dur="1000"
+    // anim.from="#eef"
+    // anim.to="#ccf"
+    // anim.fill="forwards"
+    // anim.easing="ease-in-out-quad"
+    // anim.direction="alternate"
+    // anim.repeat="indefinite"
+    // this.firstElementChild.setAttribute('material', 'color: red')
+    // this.firstElementChild.appendChild(anim)
+  }
+  function cubeLeft () {
+    // this.setAttribute('color', this.getAttribute('prev-color'))
+    // this.firstElementChild.removeChild(this.querySelector('#hover-animation-el'))
+  }
